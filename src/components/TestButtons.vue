49c7c9d262b142740308245c7848d97a5fa65903
Dem Leonid <ledemi@ttu.ee>
2023-02-10 12:46:38 +0200

    buttons is made

---
+++ src/components/TestButtons.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    setup: () => {},
+});
+</script>
+
+<template>
+    <div class="page-buttons flex justify-content-between">
+        <button class="btn btn--primary">Modal 1</button>
+        <button class="btn2">Modal 2</button>
+    </div>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
-</template>
-
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import TestButtons from "@/components/TestButtons.vue";
+import { defineComponent } from "@vue/runtime-core";
 
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+export default defineComponent({
+    components: { TestButtons },
+});
 </script>
+
+<template>
+    <div class="container">
+        <test-buttons />
+    </div>
+</template>
