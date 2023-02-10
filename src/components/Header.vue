fed1f00f4362736e494d9063317226d1616e8b65
Dem Leonid <ledemi@ttu.ee>
2023-02-10 12:15:58 +0200

    header done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,19 @@ export default defineComponent({
 <template>
     <header class="header">
         <div class="container">
-            
+            <div class="header__wrap flex align-items-center justify-content-between">
+                <div class="header__logo">
+                    <router-link to="/">
+                        <img src="/assets/Group_31391.svg" alt="logo" />
+                    </router-link>
+                </div>
+                <div class="header__body">
+                    <nav class="header__body__nav">
+                        <router-link to="/" class="header__body__nav__item" active-class="header__body__nav__item--active">Modal with Native css</router-link>
+                        <router-link to="/libs" class="header__body__nav__item" active-class="header__body__nav__item--active">Modal using libries</router-link>
+                    </nav>
+                </div>
+            </div>
         </div>
     </header>
 </template>
