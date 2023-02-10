2ab0a4bd604543d2c2853c7c1c238e4fb7a05b05
Dem Leonid <ledemi@ttu.ee>
2023-02-10 11:17:05 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-dom";
+import Header from "./components/Header.vue";
+
+export default defineComponent({
+    setup: () => {},
+    components: { Header },
+});
+</script>
+
+<template>
+    <Header />
+    <router-view />
+</template>
+
+<style lang="scss">
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+nav {
+    padding: 30px;
+
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+            color: #42b983;
+        }
+    }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    setup: () => {},
+});
+</script>
+
+<template>
+    <header class="header">
+        <div class="container">
+            
+        </div>
+    </header>
+</template>
+
+<style scoped></style>
