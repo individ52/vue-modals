0b4cd0e8e394da1783c638d456a27db57fe7a568
Dem Leonid <ledemi@ttu.ee>
2023-02-13 11:41:06 +0200

    add cards for each button

--- src/components/TestButtons.vue
+++ src/components/TestButtons.vue
@@ -4,6 +4,7 @@ import NextModal1Steps from "./NextModal1/NextModal1Steps.vue";
 import NextModal2Steps from "./NextModal2/NextModal2Steps.vue";
 import TestModalSteps from "./TestModal/TestModalSteps.vue";
 import { useI18n } from "vue-i18n";
+import Card from "./UI/Cards/Card.vue";
 export default defineComponent({
     data: () => ({
         showTest1: false,
@@ -14,16 +15,49 @@ export default defineComponent({
         const { t } = useI18n({ useScope: "global" });
         return { t };
     },
-    components: { TestModalSteps, NextModal1Steps, NextModal2Steps },
+    components: { TestModalSteps, NextModal1Steps, NextModal2Steps, Card },
 });
 </script>
 
 <template>
     <div class="page-buttons flex justify-content-center align-items-center">
-        <div class="page-buttons__wrap flex justify-content-around">
-            <button class="btn btn--primary" @click="showTest1 = !showTest1">{{ $t("buttons.button-1") }}</button>
-            <button class="next-btn next-btn--primary" @click="showTest2 = !showTest2">{{ $t("buttons.button-2") }}</button>
-            <button class="next-btn next-btn--primary" @click="showNext = !showNext">{{ $t("buttons.button-3") }}</button>
+        <div class="page-buttons__wrap flex justify-content-around flex-wrap">
+            <div class="page-buttons__item">
+                <Card>
+                    <template v-slot:title>Button 2 => Modal 2</template>
+                    <template v-slot:description
+                        >This is button 2 that opens test modal with 2 fields: valua A and value B.<br />
+                        <i>New modal design</i></template
+                    >
+                    <template v-slot:buttons>
+                        <button class="next-btn next-btn--primary" @click="showTest2 = !showTest2">{{ $t("buttons.button-2") }}</button>
+                    </template>
+                </Card>
+            </div>
+            <div class="page-buttons__item">
+                <Card>
+                    <template v-slot:title>Button 1 => Modal 1</template>
+                    <template v-slot:description
+                        >This is button 1 that opens test modal with 2 fields: valua A and value B<br />
+                        <i>Default modal design</i></template
+                    >
+                    <template v-slot:buttons
+                        ><button class="btn btn--primary" @click="showTest1 = !showTest1">{{ $t("buttons.button-1") }}</button></template
+                    >
+                </Card>
+            </div>
+            <div class="page-buttons__item">
+                <Card>
+                    <template v-slot:title>Button 3 => Modal 3</template>
+                    <template v-slot:description
+                        >This is button 3 that opens new modal with all inputs: text inputs, selects, upload files, description.<br />
+                        <i>New modal design</i></template
+                    >
+                    <template v-slot:buttons>
+                        <button class="next-btn next-btn--primary" @click="showNext = !showNext">{{ $t("buttons.button-3") }}</button>
+                    </template>
+                </Card>
+            </div>
         </div>
     </div>
     <test-modal-steps :show="showTest1" @close="showTest1 = false" />
---
+++ src/components/UI/Cards/Card.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    props: {
+        type: {
+            type: String,
+            default: "default",
+            validator(value: string) {
+                return ["default", "next"].includes(value);
+            },
+        },
+    },
+    setup: () => {},
+});
+</script>
+
+<template>
+    <div :class="`card-${type}`">
+        <div class="card">
+            <div
+                class="card__picture"
+                :style="{
+                    backgroundImage: 'url(https://lh3.googleusercontent.com/ry6NpaiU2V5hSP6A5h9Q8I-QcqtyMW1UfNSODCoZGj6x8ic-RmEasoh8rG8cUEqt-ako6jnI7qoyv5dBT0rUvxlH7IEIoWJMeTKRUvKNKA)',
+                }"
+            ></div>
+            <div class="card__content">
+                <div class="card__content__title">
+                    <slot name="title"></slot>
+                </div>
+                <div class="card__content__description">
+                    <slot name="description"></slot>
+                </div>
+                <div class="card__content__buttons flex justify-content-end align-items-center">
+                    <slot name="buttons"></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
